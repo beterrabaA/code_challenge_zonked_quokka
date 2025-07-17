8ec188618e3d08b9baad24f5338caa9258bbe940
Beterraba <ls6182315@gmail.com>
2025-07-16 23:14:27 -0300

    remove unsed packages

--- prisma/migrations/20250715232611_/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "counter" INTEGER NOT NULL DEFAULT 0,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
